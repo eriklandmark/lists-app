c580d98cee35fe88b3256668e2621523e5829212
Erik Landmark <erik@landmark.se>
2020-09-17 21:58:09 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    v-app
+
+</template>
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({
+    components: {
+
+    },
+})
+export default class App extends Vue {
+}
+</script>
+<style>
+
+</style>
